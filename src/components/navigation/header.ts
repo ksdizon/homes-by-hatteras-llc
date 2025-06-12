350a013c8feb0880dd3df47c7fbcdd12a8d4bf53
Ken Shamrock Dizon <dizonshamrock@gmail.com>
2025-06-12 08:41:26 +0800

    href to home nav button

--- src/components/navigation/header.ts
+++ src/components/navigation/header.ts
@@ -2,7 +2,7 @@ export function loadHeader() {
   document.querySelector("#header")!.innerHTML = `
   <header class="h-16 w-full flex items-center justify-between px-8 bg-slate-900 fixed top-0 z-50">
     <div id="logo-header">
-      <a href="/" class="font-inter text-white text-xl font-semibold tracking-wider">Homes By Tatteras LLC</a>
+      <a href="#hero" class="font-inter text-white text-xl font-semibold tracking-wider">Homes By Tatteras LLC</a>
     </div>
     <nav id="nav-header" class="flex gap-8">
       <a href="/" class="font-inter text-white hover:text-gray-300 transition-colors font-medium text-sm tracking-wide">Home</a>
